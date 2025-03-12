5fdf65d47e96ad3db42bb7d82a1504be9eeffd6e
princiyarodrigues <princiyarodrigues17@gmail.com>
2025-03-12 02:38:15 +0100

    File name renamed

--- src/components/DepartmentGrid.js
+++ src/components/DepartmentGrid.js
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { CategoryCard } from '../components/Category-card';
+import { CategoryCard } from './Category-card';
 
 export default function DepartmentGrid({ departments }) {
   const router = useRouter();
